d27538a63594ad3626648dc71abcfd87449b5d3b
OliverBoesen <145548550+OliverBoesen@users.noreply.github.com>
2024-10-31 11:13:40 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+// Kodet af Oliver, Andreas, Nicolaj og Nikoline
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
